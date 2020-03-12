ef112c3b13b60c2248a2755a92215ad50122aa99
mfgm2 <repom2@airmail.cc>
2020-03-12 04:43:41 +0300

    toggle on/off + custom chains

--- setup_rules.bash
+++ setup_rules.bash
@@ -35,6 +35,31 @@ CHAIN_FWD="${CHAIN}_fwd"
 CHAIN_PRE="${CHAIN}_nat_pre"
 CHAIN_PST="${CHAIN}_nat_post"
 
+FINAL_RULE=$(cfg '.final_rule') || exit 2
+INSERT_TO=$(cfg '.insert_to') || exit 2
+
+C_INP=$(cfg '.input_chain') || exit 2
+C_FWD=$(cfg '.forward_chain') || exit 2
+C_PRE=$(cfg '.nat_prerouting_chain') || exit 2
+C_PST=$(cfg '.nat_postrouting_chain') || exit 2
+
+case "$INSERT_TO" in 
+  'A') I_ARG='-A';;
+  ''|*[!0-9]*) echo "cfg: bad insert_to"; exit 2;;
+  *) I_ARG='-I'; I_IDX="$INSERT_TO";;
+esac
+
+function inject {
+  mode=$1; shift
+  table=$1; shift
+  chain=$1; shift
+
+  case "$mode" in
+    'add') iptables -t "$table" "$I_ARG" "$chain" $I_IDX $@;;
+    'del') iptables -t "$table" -D "$chain" $@;;
+  esac
+}
+
 function new_chain {
   iptables -t $1 -N $2 2>/dev/null
   iptables -t $1 -F $2
@@ -46,14 +71,18 @@ function del_chain {
 }
 
 function toggle {
-  iptables $1 OUTPUT -j $CHAIN
-  iptables $1 FORWARD -j $CHAIN_FWD
-  iptables -t nat $1 PREROUTING -j $CHAIN_PRE
-  iptables -t nat $1 POSTROUTING -j $CHAIN_PST
+  inject $1 filter $C_INP -j $CHAIN
+  inject $1 filter $C_FWD -j $CHAIN_FWD
+  inject $1 nat $C_PRE -j $CHAIN_PRE
+  inject $1 nat $C_PST -j $CHAIN_PST
 }
 
 if mode "on"; then
-  exit 137
+  toggle add
+fi
+
+if mode "off"; then 
+  toggle del
 fi
 
 if mode "setup"; then
@@ -62,7 +91,7 @@ if mode "setup"; then
   iptables -A $CHAIN -d $ENDPOINT -j ACCEPT
   iptables -A $CHAIN -d 127.0.0.0/8,192.168.0.0/16,172.16.0.0/12,10.0.0.0/8 -j ACCEPT
   iptables -A $CHAIN -m state --state RELATED,ESTABLISHED -j ACCEPT
-  iptables -A $CHAIN -j DROP
+  iptables -A $CHAIN -j "$FINAL_RULE"
 
   new_chain filter $CHAIN_FWD
   iptables -A $CHAIN_FWD -d $ENDPOINT -j ACCEPT
@@ -75,6 +104,7 @@ if mode "setup"; then
 fi
 
 if mode "clean"; then
+  toggle del
   del_chain filter $CHAIN
   del_chain filter  $CHAIN_FWD
   del_chain nat $CHAIN_PRE
